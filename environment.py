780bde1482e55b6e084eecfb5a9fb9c56bc3305b
GDcheerios <minecreeper0913@gmail.com>
2025-04-02 03:26:22 -0500

    added merch

--- environment.py
+++ environment.py
@@ -1,43 +1,52 @@
 from PSQLConnector.connector import PSQLConnection as Database
 from GPSystem import GPSystem
+from utils.printy import *
 
-print("loading environment variables...")
+print_start("loading environment variables...")
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-print("loaded environment variables")
+print_end()
 
-print("setting main variables")
+print_start("setting main variables")
 # main variables
 domain = os.environ['DOMAIN']
 port = os.environ['PORT']  # the port
 secret = os.environ['SECRET']
 is_production = os.environ['IS_PRODUCTION'] == 1
 debug = not is_production  # debugging?
+print_end()
 
+print_start("setting gq variables")
 # Gentry's Quest
 gq_rater = GPSystem().rater
 gq_version = "V"
+print_end()
 
+print_start("allocating instance variables")
 # instance variables
 bcrypt = None  # the instance of bcrypt
 socket = None  # the instance of socketio
+print_end()
 
+print_start("setting db variables")
 # DB
 db_user = os.environ['DB_USER']
 db_password = os.environ['DB_PASSWORD']
 db_hostname = os.environ['DB_HOSTNAME']
 db_port = 5432
 db = os.environ['DB']
+print_end()
 
+print_start("setting osu variables")
 # osu
 osu_secret = os.environ['OSU_SECRET']
 osu_api_key = os.environ['OSU_API_KEY']
 osu_client_id = os.environ['CLIENT_ID']
+print_end()
 
-print("loading database")
-
+print_start("loading database")
 database = Database
 database.connect(
     db_user,
@@ -45,3 +54,4 @@ database.connect(
     db_hostname,
     db
 )
+print_end()
--- routes/pages/gentrys_quest_routes.py
+++ routes/pages/gentrys_quest_routes.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 gentrys_quest_blueprint = Blueprint("gentrys_quest_blueprint", __name__)
 
 
 @gentrys_quest_blueprint.route("/")
-async def gentrys_quest_home(): return render_template("gentrys quest/home.html")
+def gentrys_quest_home(): return render_template("gentrys quest/home.html")
 
+
+@gentrys_quest_blueprint.route("/merch")
+def gentrys_quest_merch(): return redirect("https://gentrysquestshop.printify.me/")
