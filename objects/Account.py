aef38422e8fc63a967d15fa4cd5c926aa9419e4d
Brayden Messerschmidt <54640414+GDcheerios@users.noreply.github.com>
2025-07-06 23:36:05 -0500

    Account rework (#154)
    * added migration
    
    adds tags for classic users
    
    * update requirements
    
    * fix osu logic
    
    * fix up profile page
    
    * change default to offline
    
    ---------
    
    Co-authored-by: GDcheerios <minecreepper0913@gmail.com>

--- objects/Account.py
+++ objects/Account.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from environment import database
 from api.osu_api import get_user_info
 from api.gentrys_quest.user_api import get_xp
@@ -12,6 +14,9 @@ class Account:
     about: str
     pfp: str
     status: str
+    tags: list
+    gq_scores: list
+    osu_data: dict
     exists: bool
 
     def __init__(self, identifier):
@@ -22,21 +27,21 @@ class Account:
         except ValueError:
             from_query_string = "username = %s"
 
-        result = database.fetch_one(
+        result = database.fetch_to_dict(
             f"""
             SELECT 
-                id,                                                                                             -- 0
-                username,                                                                                       -- 1
-                password,                                                                                       -- 2
-                email,                                                                                          -- 3
-                osu_id,                                                                                         -- 4
-                about,                                                                                          -- 5
-                status,                                                                                         -- 6
+                id,
+                username,
+                password,
+                email,
+                osu_id,
+                about,
+                status,
                 EXISTS (
                     SELECT 1
                     FROM gq_data
                     WHERE gq_data.id = accounts.id
-                ) AS has_gq                                                                                     -- 7
+                ) AS has_gq
             FROM accounts 
             WHERE {from_query_string}
             """,
@@ -46,15 +51,17 @@ class Account:
         self.pfp = "https://storage.cloud.google.com/gdcheerioscombucket/profile-pictures/huh.png"
 
         try:
-            self.id = result[0]
-            self.username = result[1]
-            self.password = result[2]
-            self.email = result[3]
-            self.has_osu = result[4] != 0
-            self.has_gq = result[7]
-            self.osu_id = result[4]
-            self.about = result[5]
-            self.status = result[6]
+            self.id = result["id"]
+            self.username = result["username"]
+            self.password = result["password"]
+            self.email = result["email"]
+            self.has_osu = result["osu_id"] != 0
+            self.has_gq = result["has_gq"]
+            self.osu_id = result["osu_id"]
+            self.about = result["about"]
+            self.status = result["status"]
+            self.tags = database.fetch_all_to_dict("SELECT * FROM account_tags WHERE account = %s",
+                                                   params=(self.id,)) or []
             self.exists = True
         except TypeError:
             self.id = 0
@@ -66,8 +73,41 @@ class Account:
             self.has_gq = False
             self.osu_id = 0
             self.about = "This user does not exist"
-            self.status = "restricted"
+            self.status = "offline"
+            self.tags = []
             self.exists = False
+
+        self.gq_scores = []
+        self.osu_data = {}
+
+        if self.has_osu:
+            self.osu_data = self.get_osu_data()
+
+        self.gq_scores = database.fetch_all_to_dict(
+            """
+            SELECT id,
+                   score,
+                   (SELECT gq_leaderboards.name
+                    from gq_leaderboards
+                    where gq_leaderboards.id = gq_scores.leaderboard) as name
+            FROM gq_scores
+            WHERE "user" = %s
+            """,
+            params=(self.id,)
+        ) or []
+
+        leaderboards = {}
+        for score in self.gq_scores:
+            if score["name"] not in leaderboards:
+                leaderboards[score["name"]] = []
+
+        for score in self.gq_scores:
+            leaderboards[score["name"]].append({
+                "score": score["score"],
+                "id": score["id"]
+            })
+
+        self.gq_scores = leaderboards
 
     # <editor-fold desc="Modifiers">
     @staticmethod
@@ -86,7 +126,6 @@ class Account:
         )
 
         database.execute(query, params)
-        Account(username).update_osu_data()  # attempt to initialize osu data
 
     @staticmethod
     def set_status(id: int, status: str):
@@ -113,91 +152,101 @@ class Account:
         result = database.fetch_all(f"select username from accounts where username = %s;", params=(name,))
         return len(result) > 0
 
+    # </editor-fold>
+
+    # <editor-fold desc="Osu">
+
+    def fetch_osu_data(self):
+        if self.has_osu:
+            try:
+                data = get_user_info(self.osu_id)
+                return {
+                    'username': data['username'],
+                    'score': data['statistics']['total_score'],
+                    'playcount': data['statistics']['play_count'],
+                    'accuracy': data['statistics']['hit_accuracy'],
+                    'performance': data['statistics']['pp'],
+                    'rank': data['statistics']['global_rank'],
+                }
+            except KeyError:
+                print("User doesn't exist on osu!")
+                database.execute("UPDATE accounts SET osu_id = 0 WHERE id = %s", params=(self.id,))
+
+        return None
+
     def get_osu_data(self):
         if self.has_osu:
-            data = get_user_info(self.osu_id)
-            return {
-                'username': data['username'],
-                'score': data['statistics']['total_score'],
-                'playcount': data['statistics']['play_count'],
-                'accuracy': data['statistics']['hit_accuracy'],
-                'performance': data['statistics']['pp'],
-                'rank': data['statistics']['global_rank']
-            }
-        else:
-            return None
+            data = database.fetch_to_dict("SELECT * FROM osu_users WHERE id = %s", params=(self.osu_id,))
+            if not data:
+                if not self.update_osu_data():
+                    return None
+                return self.get_osu_data()
+            else:
+                return data
+
+        return None
 
     def update_osu_data(self):
-        data = self.get_osu_data()
-        if data is not None:
-            result = database.fetch_one("SELECT id FROM osu_users WHERE id = %s", params=(self.osu_id,))
-            if result:
-                database.execute(
-                    "UPDATE osu_users SET username = %s, score = %s, playcount = %s, accuracy = %s, performance = %s, rank = %s WHERE id = %s",
-                    params=(
-                        data['username'],
-                        data['score'],
-                        data['playcount'],
-                        data['accuracy'],
-                        data['performance'],
-                        data['rank'],
-                        self.osu_id
-                    )
+        data = None
+
+        try:
+            last_refresh = \
+                database.fetch_one("SELECT last_refresh FROM osu_users WHERE id = %s", params=(self.osu_id,))[0]
+            right_now = (last_refresh + timedelta(minutes=1))
+            can_refresh = right_now < datetime.now()
+            print(f"last refresh: {last_refresh}\n"
+                  f"now: {right_now}\n"
+                  f"{can_refresh}")
+        except TypeError:
+            can_refresh = True
+
+        if can_refresh:
+            data = self.fetch_osu_data()
+            if not data:
+                return False
+
+        result = database.fetch_one("SELECT id FROM osu_users WHERE id = %s", params=(self.osu_id,))
+        if result:
+            database.execute(
+                "UPDATE osu_users SET username = %s, score = %s, playcount = %s, accuracy = %s, performance = %s, rank = %s, last_refresh = now() WHERE id = %s",
+                params=(
+                    data['username'],
+                    data['score'],
+                    data['playcount'],
+                    data['accuracy'],
+                    data['performance'],
+                    data['rank'],
+                    self.osu_id
                 )
-            else:
-                database.execute(
-                    "INSERT INTO osu_users (id, username, score, playcount, accuracy, performance, rank) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                    params=(
-                        self.osu_id,
-                        data['username'],
-                        data['score'],
-                        data['playcount'],
-                        data['accuracy'],
-                        data['performance'],
-                        data['rank']
-                    )
+            )
+        else:
+            database.execute(
+                "INSERT INTO osu_users (id, username, score, playcount, accuracy, performance, rank) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                params=(
+                    self.osu_id,
+                    data['username'],
+                    data['score'],
+                    data['playcount'],
+                    data['accuracy'],
+                    data['performance'],
+                    data['rank']
                 )
+            )
+
+        return True
 
     # </editor-fold>
 
     def jsonify(self) -> dict:
-
-        gq_data = None
-        items = {
-            "characters": [],
-            "artifacts": [],
-            "weapons": []
-        }
-        osu_data = None
-
-        if self.has_gq:
-            gq_data = database.fetch_to_dict("SELECT * FROM gq_data WHERE id = %s", params=(self.id,))
-            # gq_data.pop['xp']
-            gq_data['experience'] = get_xp(self.id)
-            gq_items = database.fetch_all_to_dict("SELECT * FROM gq_items WHERE owner = %s", params=(self.id,))
-
-            for item in gq_items:
-                if item['type'] == 'character':
-                    items['characters'].append(item)
-
-                elif item['type'] == 'artifact':
-                    items['artifacts'].append(item)
-
-                else:
-                    items['weapons'].append(item)
-
-            gq_data['items'] = items
-
-        if self.has_osu:
-            osu_data = database.fetch_to_dict("SELECT * FROM osu_users WHERE id = %s", params=(self.osu_id,))
-
         return {
             "id": self.id,
             "username": self.username,
             "email": self.email,
-            "osu data": osu_data,
-            "gq data": gq_data,
+            "osu data": self.osu_data,
+            "gq data": None,
+            "gq scores": self.gq_scores,
             "about": self.about,
             "pfp": self.pfp,
-            "status": self.status
+            "status": self.status,
+            "tags": self.tags
         }
