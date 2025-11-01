54542f85c6d79b4f16719b49227b24dac62a10b3
Brayden Messerschmidt <minecreeper0913@gmail.com>
2025-11-01 15:54:15 -0500

    merge changes into dev

---
+++ utils/bucket_helper.py
@@ -0,0 +1,6 @@
+import environment
+
+changelogs = environment.bucket_path + "/changelogs"
+gentrys_quest_resources = environment.bucket_path + "/gentrys-quest-resources"
+pictures = environment.bucket_path + "/pictures"
+profile_pictures = environment.bucket_path + "/profile-pictures"
