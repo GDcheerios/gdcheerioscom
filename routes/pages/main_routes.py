f3728cf07edecb538a591504031ce120298449f3
Brayden Messerschmidt <minecreeper0913@gmail.com>
2026-01-20 06:42:38 -0600

    added search feature to website (#204)

--- routes/pages/main_routes.py
+++ routes/pages/main_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, request
 
 import environment
 from api.gentrys_quest.leaderboard_api import *
+from api.account_api import Account
 
 main_blueprint = Blueprint("main_blueprint", __name__)
 
@@ -13,6 +14,25 @@ def index(): return render_template("index.html")
 
 @main_blueprint.route("/about")
 def about(): return render_template("about.html")
+
+
+@main_blueprint.route("/search")
+def search(): return render_template("search.html")
+
+
+@main_blueprint.get("/search/results")
+def search_results():
+    query = request.args.get("q", "")
+    if not query:
+        return redirect("/search")
+
+    users_result = Account.search(query)
+    osu_result = environment.database.fetch_all_to_dict("select * from osu_matches where name ilike %s limit 5",
+                                                        params=(f"%{query}%",))
+    return {
+        "users": users_result,
+        "matches": osu_result
+    }
 
 
 @main_blueprint.route("/supporter")
