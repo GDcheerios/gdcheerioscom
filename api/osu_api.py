53744e2831fb441d555a2b06a6e5645fd0b25761
Brayden Messerschmidt <minecreeper0913@gmail.com>
2026-09-22 20:30:28 -0500

    implement new osu schema (#259)

--- api/osu_api.py
+++ api/osu_api.py
@@ -1,6 +1,7 @@
 import requests
 import datetime as dt
 from datetime import timezone
+import traceback
 
 import environment
 from utils.logger import setup_logger
@@ -51,62 +52,73 @@ def get_user_info(user_identifier):
     Otherwise, it is treated as a username.
     """
 
-    try:
-        osu_id = int(user_identifier)
-        is_id = True
-    except (TypeError, ValueError):
-        osu_id = None
-        is_id = False
+    user_check = environment.database.fetch_to_dict(
+        """
+        SELECT *
+        FROM osu.users
+        WHERE id::text = %s
+           OR username::text = %s
+        """,
+        params=(str(user_identifier), str(user_identifier))
+    )
 
-    if is_id:
-        user_check = environment.database.fetch_to_dict(
-            "SELECT * FROM osu.users WHERE id = %s",
-            params=(osu_id,)
+    if (
+            not user_check
+            or user_check["last_refresh"]
+            <= dt.datetime.now(tz=timezone.utc) - dt.timedelta(minutes=2)
+    ):
+        logger.info("getting osu user %s", user_identifier)
+        user_req = requests.get(
+            f"https://osu.ppy.sh/api/v2/users/{user_identifier}/osu",
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {check_access()}",
+            },
+        ).json()
+        recent_score_req = requests.get(
+            f"https://osu.ppy.sh/api/v2/users/{user_req['id']}/scores/recent",
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {check_access()}",
+                "x-api-version": "20220705"
+            },
+            params={
+                "mode": "osu",
+                "limit": 1,
+                "include_fails": "1"
+            }
+        ).json()
+
+        if len(recent_score_req) == 0:
+            return {
+                'user': user_req,
+                'score': None
+            }
+
+        return {
+            'user': user_req,
+            'score': recent_score_req[0]
+        }
+
+    if user_check:
+        score_check = environment.database.fetch_to_dict(
+            """
+            SELECT *
+            FROM osu.scores
+            WHERE user_id = %s
+            ORDER BY submitted_at DESC
+            LIMIT 1
+            """,
+            (user_check['id'],)
         )
+        return {
+            'user': user_check,
+            'score': score_check
+        }
 
-        if (
-                not user_check
-                or user_check["last_refresh"]
-                <= dt.datetime.now(tz=timezone.utc) - dt.timedelta(minutes=1)
-        ):
-            logger.info("getting osu user by id %s", osu_id)
-            return requests.get(
-                f"https://osu.ppy.sh/api/v2/users/{osu_id}/osu",
-                headers={
-                    "Accept": "application/json",
-                    "Content-Type": "application/json",
-                    "Authorization": f"Bearer {check_access()}",
-                },
-            ).json()
-
-        if user_check:
-            return user_check
-
-    else:
-        username = str(user_identifier)
-        logger.info("getting osu user by username %s", username)
-        user_check = environment.database.fetch_to_dict(
-            "SELECT * FROM osu.users WHERE username = %s",
-            params=(username,)
-        )
-
-        if (
-                not user_check
-                or user_check["last_refresh"]
-                <= dt.datetime.now(tz=timezone.utc) - dt.timedelta(minutes=1)
-        ):
-            return requests.get(
-                f"https://osu.ppy.sh/api/v2/users/{username}/osu",
-                headers={
-                    "Accept": "application/json",
-                    "Content-Type": "application/json",
-                    "Authorization": f"Bearer {check_access()}",
-                },
-            ).json()
-        else:
-            return user_check
-
-    logger.info("user %s already checked within last minute or user not found", osu_id)
+    logger.info("user %s already checked within last minute or user not found", user_identifier)
 
     return None
 
@@ -119,6 +131,8 @@ def extract_info(data):
     :return: json of important user info
     """
 
+    print(data)
+
     try:
         try:
             if data['error'] is None:
@@ -127,34 +141,58 @@ def extract_info(data):
             pass
 
         if data:
+            user_info = data['user']
             extracted_info = {
-                'id': data['id'],
-                'username': data['username'],
-                'score': data['statistics']['total_score'],
-                'playcount': data['statistics']['play_count'],
-                'accuracy': data['statistics']['hit_accuracy'],
-                'performance': data['statistics']['pp'],
-                'rank': data['statistics']['global_rank'],
-                'avatar': data['avatar_url'],
-                'background': data['cover_url']
+                'id': user_info['id'],
+                'username': user_info['username'],
+                'total_score': user_info['statistics']['total_score'],
+                'ranked_score': user_info['statistics']['ranked_score'],
+                'total_hits': user_info['statistics']['total_hits'],
+                'playcount': user_info['statistics']['play_count'],
+                'accuracy': user_info['statistics']['hit_accuracy'],
+                'pp': user_info['statistics']['pp'],
+                'global_rank': user_info['statistics']["global_rank"],
+                'country_rank': user_info['statistics']['country_rank'],
+                'grade_ss': user_info['statistics']['grade_counts']['ss'],
+                'grade_ssh': user_info['statistics']['grade_counts']['ssh'],
+                'grade_s': user_info['statistics']['grade_counts']['s'],
+                'grade_sh': user_info['statistics']['grade_counts']['sh'],
+                'grade_a': user_info['statistics']['grade_counts']['a'],
+                'avatar': user_info['avatar_url'],
+                'background': user_info['cover_url']
             }
 
             db = environment.database
-            existing = db.fetch_one(
-                "SELECT id FROM osu.users WHERE id = %s",
-                params=(data['id'],)
-            )
+            if data['score'] is None:
+                data['score'] = {"id": 0}
 
-            if existing:
+            exists = db.fetch_to_dict(
+                """
+                SELECT EXISTS(SELECT 1 FROM osu.users WHERE id = %s)          as "user",
+                       EXISTS(SELECT 1 FROM osu.scores WHERE id = %s) as score
+                """,
+                params=(data['user']['id'], data['score']['id'])
+            )
+            print(exists)
+
+            if exists['user']:
                 db.execute(
                     """
                     UPDATE osu.users
                     SET username     = %s,
-                        score        = %s,
+                        total_score  = %s,
+                        ranked_score = %s,
+                        total_hits   = %s,
                         playcount    = %s,
                         accuracy     = %s,
-                        performance  = %s,
-                        rank         = %s,
+                        pp           = %s,
+                        global_rank  = %s,
+                        country_rank = %s,
+                        grade_ss     = %s,
+                        grade_ssh    = %s,
+                        grade_s      = %s,
+                        grade_sh     = %s,
+                        grade_a      = %s,
                         avatar       = %s,
                         background   = %s,
                         last_refresh = now()
@@ -162,11 +200,19 @@ def extract_info(data):
                     """,
                     params=(
                         extracted_info['username'],
-                        extracted_info['score'],
+                        extracted_info['total_score'],
+                        extracted_info['ranked_score'],
+                        extracted_info['total_hits'],
                         extracted_info['playcount'],
                         extracted_info['accuracy'],
-                        extracted_info['performance'],
-                        extracted_info['rank'],
+                        extracted_info['pp'],
+                        extracted_info['global_rank'],
+                        extracted_info['country_rank'],
+                        extracted_info['grade_ss'],
+                        extracted_info['grade_ssh'],
+                        extracted_info['grade_s'],
+                        extracted_info['grade_sh'],
+                        extracted_info['grade_a'],
                         extracted_info['avatar'],
                         extracted_info['background'],
                         extracted_info['id']
@@ -176,28 +222,54 @@ def extract_info(data):
                 db.execute(
                     """
                     INSERT INTO osu.users
-                    (id, username, score, playcount, accuracy, performance, rank, avatar, background, last_refresh)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, now())
+                    (id, username, total_score, ranked_score, total_hits, playcount, accuracy, pp, global_rank,
+                     country_rank, grade_ss, grade_ssh, grade_s, grade_sh, grade_a, avatar, background, last_refresh)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, now())
                     """,
                     params=(
                         extracted_info['id'],
                         extracted_info['username'],
-                        extracted_info['score'],
+                        extracted_info['total_score'],
+                        extracted_info['ranked_score'],
+                        extracted_info['total_hits'],
                         extracted_info['playcount'],
                         extracted_info['accuracy'],
-                        extracted_info['performance'],
-                        extracted_info['rank'],
+                        extracted_info['pp'],
+                        extracted_info['global_rank'],
+                        extracted_info['country_rank'],
+                        extracted_info['grade_ss'],
+                        extracted_info['grade_ssh'],
+                        extracted_info['grade_s'],
+                        extracted_info['grade_sh'],
+                        extracted_info['grade_a'],
                         extracted_info['avatar'],
                         extracted_info['background'],
                     )
                 )
 
-            return extracted_info
+            if not exists["score"] and data['score']['id'] != 0:
+                db.execute(
+                    """
+                    INSERT INTO osu.scores (id, beatmap_id, user_id, submitted_at, accuracy, rank, pp, score)
+                    VALUES (%s, %s, %s, now(), %s, %s, %s, %s)
+                    """,
+                    params=(
+                        data['score']['id'],
+                        data['score']['beatmap']['id'],
+                        data['user']['id'],
+                        data['score']['accuracy'],
+                        data['score']['rank'],
+                        data['score']['pp'],
+                        data['score']['classic_total_score'],
+                    )
+                )
+
+            return data['user']
         else:
             logger.warning("user info not found")
             return None
     except KeyError as e:
-        return data
+        return data['user']
 
 
 def fetch_osu_data(user_id):
